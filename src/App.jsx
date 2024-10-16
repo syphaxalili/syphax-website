d03480c20c5689cf37c2dff485a2138d255e57c2
Syphax <sif7gmr@gmail.com>
2024-10-16 15:08:35 +0200

    installed serve on the project

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
 import Services from "./components/services/Services";
 import Qualification from "./components/qualification/Qualification";
-import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import Scrollup from "./components/scrollup/Scrollup";
